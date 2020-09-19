2502456237f092cc7494e54f15028411b115f58f
daniel.monroy <daniel.monroym@udea.edu.co>
2020-09-19 09:50:13 -0500

    First commit

---
+++ src/components/ImageList.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col v-for="item of items.hits" v-bind:key="item.id" sm="4" cols="12">
+        <v-card>
+        <img :src="item.previewURL" alt="Imagen">
+          <v-card-title>{{item.user}}</v-card-title>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "ImageList",
+  props: ["items"],
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/Toolbar.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <v-app-bar color="deep-purple accent-4" dense dark>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Mi Galeria</v-toolbar-title>
+    </v-app-bar>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
